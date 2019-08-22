2a1b3dffdf342ff5d5112c9f1be943d31664b9d0
Bahay Gulle Bilgi and Misaki Koonce <bahay@>
2019-08-22 15:48:16 -0700

    Add 4 digits and 3 digits conditions.

--- js/scripts.js
+++ js/scripts.js
@@ -9,65 +9,105 @@ function concatRomans(letter, number) {
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
-    var userInput = parseInt($("input#user-input").val());
+    var userInput = $("input#user-input").val();
+    // console.log(userInput);
+    var digits = userInput.split('').length;
+    // console.log(digits);
 
-    if (userInput > 3999) {
-      alert("Number is too big. Enter number less than 4000!")
-    } else if (userInput >= 1000) {
-      var thousands = userInput / 1000;
-      concatRomans("M", thousands);
-      console.log(concatRomans("M", thousands));
+    if (userInput > 3999 || userInput <= 0) {
+      alert("Please enter number between 1 to 3999!")
+    }
 
-    } else if (userInput >= 900) {
-      // concatRomans("CM", 1);
-      console.log("CM");
+    var thousandsRem, hundredsRem, tens;
 
-    } else if (userInput >= 500) {
-      var handreds2 = userInput / 100 - 5;
-      var numbsC = concatRomans("C", handreds2);
-      console.log("D" + numbsC);
 
-    } else if (userInput >= 400) {
-      console.log("CD");
 
-    } else if (userInput >= 100) {
-      var handreds1 = userInput / 100;
-      var numbsD = concatRomans("D", handreds1);
-      console.log(numbsD);
+    if (digits >= 4) {
+      thousandsRem = userInput % 1000;
+      var digitThousands = (userInput - thousandsRem) / 1000;
+      var numbsM = concatRomans("M", digitThousands);
+      console.log(numbsM);
+    }
+    if (digits >= 3) {
+      hundredsRem = thousandsRem % 100;
+      var digitHundreds = (thousandsRem - hundredsRem) / 100;
+      if (digitHundreds  === 9){
+        console.log("CM");
 
-    } else if (userInput >= 90) {
-      console.log("XC")
+      } else if (digitHundreds >= 5) {
+        var digitHundreds2= digitHundreds - 5
+        var numbsC = concatRomans("C", digitHundreds2);
+        console.log("D" + numbsC);
 
-    } else if (userInput >= 50) {
-      var tens2 = userInput / 10 - 5;
-      var numbsX = concatRomans("X", tens2);
-      console.log("L" + numbsX);
+      } else if (digitHundreds === 4) {
+        console.log("CD");
 
-    } else if (userInput >= 40) {
-      console.log("XL");
+      } else {
+        var numbsC = concatRomans("C", digitHundreds);
+        console.log(numbsC);
+      }
 
-    } else if (userInput >= 10) {
-      var tens1 = userInput / 10;
-      var numbsX = concatRomans("X", tens1);
-      console.log(numbsX);
+      // var numbsD = concatRomans("D",hundredsRem);
+      // console.log(hundredsRem);
 
-    } else if (userInput === 9) {
-      console.log("IX");
-
-    } else if (userInput >= 5) {
-      var ones2 = userInput - 5;
-      var numbsI = concatRomans("I", ones2)
-      console.log("V" + numbsI);
-
-    } else if (userInput === 4) {
-      console.log("IV");
-
+    } else if (digits >= 2) {
+      var tens = userInput % 10;
+      console.log(tens);
     } else {
-      var numbsI = concatRomans("I", userInput);
-      console.log(numbsI);
+      console.log(userInput);
     }
 
 
+    // Code below is version 1
+
+    // } else if (userInput >= 900) {
+    //   // concatRomans("CM", 1);
+    //   console.log("CM");
+    //
+    // } else if (userInput >= 500) {
+    //   var handreds2 = userInput / 100 - 5;
+    //   var numbsC = concatRomans("C", handreds2);
+    //   console.log("D" + numbsC);
+    //
+    // } else if (userInput >= 400) {
+    //   console.log("CD");
+    //
+    // } else if (userInput >= 100) {
+    //   var handreds1 = userInput / 100;
+    //   var numbsD = concatRomans("D", handreds1);
+    //   console.log(numbsD);
+    //
+    // } else if (userInput >= 90) {
+    //   console.log("XC")
+    //
+    // } else if (userInput >= 50) {
+    //   var tens2 = userInput / 10 - 5;
+    //   var numbsX = concatRomans("X", tens2);
+    //   console.log("L" + numbsX);
+    //
+    // } else if (userInput >= 40) {
+    //   console.log("XL");
+    //
+    // } else if (userInput >= 10) {
+    //   var tens1 = userInput / 10;
+    //   var numbsX = concatRomans("X", tens1);
+    //   console.log(numbsX);
+    //
+    // } else if (userInput === 9) {
+    //   console.log("IX");
+    //
+    // } else if (userInput >= 5) {
+    //   var ones2 = userInput - 5;
+    //   var numbsI = concatRomans("I", ones2)
+    //   console.log("V" + numbsI);
+    //
+    // } else if (userInput === 4) {
+    //   console.log("IV");
+    //
+    // } else {
+    //   var numbsI = concatRomans("I", userInput);
+    //   console.log(numbsI);
+    // }
 
   //  $(".result").show();
   });
